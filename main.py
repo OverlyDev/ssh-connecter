fad87011fe3743ee16bc46434ab1716b2881ffe6
kkeekk <kruse.kyle2@gmail.com>
2020-01-31 21:16:04 -0800

    wip

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ def connect_ssh(target_user, target_ip):
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     ssh.load_host_keys("public.key")
 
+    # TODO: Connect hangs. If force stopped, the exceptions will print as they should but for some reason the dang
+    #  thing hangs and I don't know why. Further investigation required.
+    #  Also, I set all the timeouts to 2 in hopes of it actually timing out but that's not the case.
     try:
         ssh.connect(hostname=target_ip, username=target_user, key_filename="./public.key", auth_timeout=2, timeout=2,
                     banner_timeout=2)
